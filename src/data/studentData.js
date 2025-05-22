f8e3d99ddfd8c92a09da6e6298e200939c82b48d
shubham kumar maurya <shubhamkumar234@lpu.in>
2025-05-22 19:58:52 +0530

    all1

--- src/data/studentData.js
+++ src/data/studentData.js
@@ -1,17 +1,1 @@
-const studentData = [
-    { id: 1, class: 1, school: "School", behavior: "Good", gpa: 9.0, attendance: "95%" },
-    { id: 2, class: 2, school: "School", behavior: "Good", gpa: 9.0, attendance: "94%" },
-    { id: 3, class: 3, school: "School", behavior: "Good", gpa: 9.0, attendance: "93%" },
-    { id: 4, class: 4, school: "School", behavior: "Good", gpa: 9.0, attendance: "92%" },
-    { id: 5, class: 5, school: "School", behavior: "Good", gpa: 9.0, attendance: "91%" },
-    { id: 6, class: 6, school: "Secondary School", behavior: "Good", gpa: 9.0, attendance: "90%" },
-    { id: 7, class: 7, school: "Secondary School", behavior: "Good", gpa: 9.0, attendance: "89%" },
-    { id: 8, class: 8, school: "Secondary School", behavior: "Good", gpa: 9.0, attendance: "88%" },
-    { id: 9, class: 9, school: "Higher Secondary School", behavior: "Good", gpa: 9.0, attendance: "87%" },
-    { id: 10, class: 10, school: "Higher Secondary School", behavior: "Good", gpa: 9.0, attendance: "86%" },
-    { id: 11, class: 11, school: "Inter Mediate", behavior: "Bad", gpa: 9.0, attendance: "85%" },
-    { id: 12, class: 12, school: "Inter Mediate", behavior: "Good", gpa: 9.0, attendance: "84%" },
-    { id: 13, course:"BCA", school: "College", behavior: "Good", gpa: 5.9, attendance: "39.5%" },
-    { id: 12, course:"MCA", school: "College", behavior: "Good", gpa: 9.3, attendance: "39%" }
-  ];
-export default studentData;
+const studentData=[{id:1,class:1,school:"School",behavior:"Good",gpa:9,attendance:"95%"},{id:2,class:2,school:"School",behavior:"Good",gpa:9,attendance:"94%"},{id:3,class:3,school:"School",behavior:"Good",gpa:9,attendance:"93%"},{id:4,class:4,school:"School",behavior:"Good",gpa:9,attendance:"92%"},{id:5,class:5,school:"School",behavior:"Good",gpa:9,attendance:"91%"},{id:6,class:6,school:"Secondary School",behavior:"Good",gpa:9,attendance:"90%"},{id:7,class:7,school:"Secondary School",behavior:"Good",gpa:9,attendance:"89%"},{id:8,class:8,school:"Secondary School",behavior:"Good",gpa:9,attendance:"88%"},{id:9,class:9,school:"Higher Secondary School",behavior:"Good",gpa:9,attendance:"87%"},{id:10,class:10,school:"Higher Secondary School",behavior:"Good",gpa:9,attendance:"86%"},{id:11,class:11,school:"Inter Mediate",behavior:"Bad",gpa:9,attendance:"85%"},{id:12,class:12,school:"Inter Mediate",behavior:"Good",gpa:9,attendance:"84%"},{id:13,course:"BCA",school:"College",behavior:"Good",gpa:5.9,attendance:"39.5%"},{id:12,course:"MCA",school:"College",behavior:"Good",gpa:9.3,attendance:"39%"}];export default studentData;
